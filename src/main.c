6db4faf796d1e52fdc8b722995d31e3c0172f72e
cynumini <cynumini@proton.me>
2026-06-26 19:48:29 +0300

    re: draw square

--- src/main.c
+++ src/main.c
@@ -76,18 +76,31 @@ int main(void) {
 
     LOG_INFO("OpenGL version is %s", glGetString(GL_VERSION));
 
-    float positions[] = {
+    GLfloat positions[] = {
         -0.5, -0.5, //
-        0.0,  0.5,  //
-        0.5,  -0.5  //
+        0.5,  -0.5, //
+        0.5,  0.5,  //
+        -0.5, 0.5   //
+    };
+
+    GLuint indices[] = {
+        0, 1, 2, //
+        2, 3, 0  //
     };
 
     GLuint buffer;
     glGenBuffers(1, &buffer);
     glBindBuffer(GL_ARRAY_BUFFER, buffer);
     glBufferData(GL_ARRAY_BUFFER, sizeof(positions), positions, GL_STATIC_DRAW);
+
     glEnableVertexAttribArray(0);
     glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 2, 0);
+
+    GLuint ibo;
+    glGenBuffers(1, &ibo);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices,
+                 GL_STATIC_DRAW);
 
     const char vertex_shader[] = {
 #embed "basic.vert.glsl"
@@ -104,7 +117,7 @@ int main(void) {
     while (!glfwWindowShouldClose(window)) {
         glClear(GL_COLOR_BUFFER_BIT);
 
-        glDrawArrays(GL_TRIANGLES, 0, ARRAY_LEN(positions) / 2);
+        glDrawElements(GL_TRIANGLES, ARRAY_LEN(indices), GL_UNSIGNED_INT, 0);
 
         glfwSwapBuffers(window);
         glfwPollEvents();
